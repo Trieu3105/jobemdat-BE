b851715fe66f6c8ec8ed05aac3af47e45483eb57
I don't know code <tmlnguyen3105@gmail.com>
2025-05-10 00:34:41 +0700

    first commit

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 // index.js
 const express = require("express");
 const app = express();
-const cors = require('cors');
-require('dotenv').config({
-    path: process.env.NODE_ENV === 'production' ? '.env.production' : '.env.development'
-  });
+const cors = require("cors");
+require("dotenv").config({
+  path:
+    process.env.NODE_ENV === "production"
+      ? ".env.production"
+      : ".env.development",
+});
 const loginRouter = require("./API/auth");
 const giftcodeRouter = require("./API/giftcode");
 const paymentRouter = require("./API/payment");
@@ -12,24 +15,26 @@ const contentRouter = require("./API/posts");
 
 const PORT = process.env.PORT || 8080;
 
-corsOptions = {
-origin: process.env.CLIENT_URL, // Allow requests from the frontend
-    credentials: true, // Allow cookies to be sent
-    methods: ["GET", "POST", "OPTIONS"], // Allow specific methods
-    allowedHeaders: ["Content-Type", "Authorization"], // Allow specific headers
-};
+router.use(
+  cors({
+    origin: "https://jobemdat-fe.vercel.app", // Đảm bảo chỉ cho phép URL frontend của bạn
+    credentials: true, // Cho phép cookie được gửi cùng yêu cầu
+    methods: ["GET", "POST", "OPTIONS"], // Cho phép các phương thức cụ thể
+    allowedHeaders: ["Content-Type", "Authorization"], // Cho phép các header cụ thể
+  })
+);
 
 app.use(cors(corsOptions)); // Apply CORS options globally
 app.use(express.json());
 
-app.get('/api/ping', (req, res) => {
-  res.json({ message: 'pong' });
+app.get("/api/ping", (req, res) => {
+  res.json({ message: "pong" });
 });
 
-app.use('/api/', loginRouter);
-app.use('/api/', giftcodeRouter);
-app.use('/api/', paymentRouter);
-app.use('/api/', contentRouter);
+app.use("/api/", loginRouter);
+app.use("/api/", giftcodeRouter);
+app.use("/api/", paymentRouter);
+app.use("/api/", contentRouter);
 
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
