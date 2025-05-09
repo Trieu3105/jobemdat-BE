5af51a79a00f1bd2a519f58eb65d039875b590e8
I don't know code <tmlnguyen3105@gmail.com>
2025-05-10 00:38:47 +0700

    first commit

--- server.js
+++ server.js
@@ -1,40 +1,35 @@
 // index.js
 const express = require("express");
 const app = express();
-const cors = require("cors");
-require("dotenv").config({
-  path:
-    process.env.NODE_ENV === "production"
-      ? ".env.production"
-      : ".env.development",
-});
+const cors = require('cors');
+require('dotenv').config({
+    path: process.env.NODE_ENV === 'production' ? '.env.production' : '.env.development'
+  });
 const loginRouter = require("./API/auth");
 const giftcodeRouter = require("./API/giftcode");
 const paymentRouter = require("./API/payment");
 const contentRouter = require("./API/posts");
 
+
 const PORT = process.env.PORT || 8080;
 
-router.use(
-  cors({
-    origin: process.env.CLIENT_URL, // Đảm bảo chỉ cho phép URL frontend của bạn
-    credentials: true, // Cho phép cookie được gửi cùng yêu cầu
-    methods: ["GET", "POST", "OPTIONS"], // Cho phép các phương thức cụ thể
-    allowedHeaders: ["Content-Type", "Authorization"], // Cho phép các header cụ thể
-  })
-);
+corsOptions = {
+  origin: process.env.CLIENT_URL, // Use CLIENT_URL from environment variables
+  methods: ['GET', 'POST',"OPTIONS"], 
+  credentials: true, // Allow credentials (cookies) to be sent
+};
 
 app.use(cors(corsOptions)); // Apply CORS options globally
 app.use(express.json());
 
-app.get("/api/ping", (req, res) => {
-  res.json({ message: "pong" });
+app.get('/api/ping', (req, res) => {
+  res.json({ message: 'pong' });
 });
 
-app.use("/api/", loginRouter);
-app.use("/api/", giftcodeRouter);
-app.use("/api/", paymentRouter);
-app.use("/api/", contentRouter);
+app.use('/api/', loginRouter);
+app.use('/api/', giftcodeRouter);
+app.use('/api/', paymentRouter);
+app.use('/api/', contentRouter);
 
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
